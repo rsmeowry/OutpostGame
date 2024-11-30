ab683cff5d28aff677bd7c58a43086a2582520c7
maxus <maxus.info.mail@gmail.com>
2024-11-30 22:49:39 +0300

    very bwa

---
+++ Assets/Scripts/UI/Interior/Res/ResourceViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Game.Production.Products;
+using UnityEngine;
+
+namespace UI.Interior.Res
+{
+    public class ResourceViewComponent: MonoBehaviour
+    {
+        [SerializeField]
+        private SingleResourceView singlePrefab;
+        [SerializeField]
+        private RectTransform container;
+
+        public void Start()
+        {
+            foreach (var res in ProductRegistry.Instance.AllItems())
+            {
+                var o = Instantiate(singlePrefab, container);
+                o.Item = res;
+            }
+            
+            var anch = container.anchoredPosition;
+            anch.y -= container.rect.height;
+            container.anchoredPosition = anch;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Interior/Res/SingleResourceView.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Game.Production.Products;
+using Game.State;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Interior.Res
+{
+    public class SingleResourceView: MonoBehaviour
+    {
+        [SerializeField]
+        private Image itemIcon;
+        [SerializeField]
+        private TMP_Text itemName;
+        [SerializeField]
+        private TMP_Text itemCount;
+        
+        [NonSerialized]
+        public StateKey Item;
+
+        public void Start()
+        {
+            var data = ProductRegistry.Instance.GetProductData(Item);
+            itemIcon.sprite = data.icon;
+            itemName.text = data.name;
+            
+            GameStateManager.Instance.onProductChanged.AddListener(HandleChangedResource);
+            PollChanges();
+        }
+
+        private void OnDisable()
+        {
+            GameStateManager.Instance.onProductChanged.RemoveListener(HandleChangedResource);
+        }
+
+        private void PollChanges()
+        {
+            itemCount.text = GameStateManager.Instance.PlayerProductCount.GetValueOrDefault(Item.Formatted(), 0) + " шт.";
+        }
+
+        private void HandleChangedResource(ProductChangedData changed)
+        {
+            if (changed.Product != Item)
+                return;
+            PollChanges();
+        }
+     }
+}
